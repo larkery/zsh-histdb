bb38214102ae4b53334bed643ed76d97b4bb855d
Robin Björklin <robin.bjorklin@gmail.com>
2022-09-06 22:21:18 -0700

    add top_here_and_now autosuggest strategy

--- autosuggest-strategy.zsh
+++ autosuggest-strategy.zsh
@@ -61,3 +61,40 @@ ORDER BY
 LIMIT 1"
     suggestion=$(_histdb_query "$query")
 }
+
+# This query will find the most recently issued command that is issued in
+# the current directory or any subdirectory preferring commands in the current session.
+_zsh_autosuggest_strategy_histdb_top_here_and_now() {
+    local query="
+SELECT
+    commands.argv
+FROM
+    history
+LEFT JOIN
+    commands
+ON
+    history.command_id = commands.id
+LEFT JOIN
+    places
+ON
+    history.place_id = places.id
+WHERE
+    places.dir
+LIKE
+    '$(sql_escape $PWD)%'
+AND
+    commands.argv
+LIKE
+    '$(sql_escape $1)%'
+AND
+    history.exit_status = 0
+GROUP BY
+    commands.argv,
+    history.session,
+    history.start_time
+ORDER BY
+    history.session = '$(sql_escape $HISTDB_SESSION)' DESC,
+    history.start_time DESC
+LIMIT 1"
+    suggestion=$(_histdb_query "$query")
+}
